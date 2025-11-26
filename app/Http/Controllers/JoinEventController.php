cde9f23f5d5bef28a628cea16b24a90938f6c4ff
Sakib9644 <17203075@iubat.edu>
2025-11-26 17:23:35 +0600

    last_commit

--- app/Http/Controllers/JoinEventController.php
+++ app/Http/Controllers/JoinEventController.php
@@ -40,54 +40,53 @@ class JoinEventController extends Controller
         return response()->json([
             'success' => true,
             'message' => 'Events retrieved successfully',
-            'data' => $events
+            'join_count' => $user->joinedevent()->count()
+            'data' => $events,
+
         ]);
     }
     public function join(Request $request, $eventId)
-{
-    $user = auth('api')->user();
+    {
+        $user = auth('api')->user();
 
-    if (!$user) {
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthenticated.'
+            ], 401);
+        }
+
+        $event = Event::find($eventId);
+        if (!$event) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Event not found.'
+            ], 404);
+        }
+
+        $alreadyJoined = JoinEvent::where('user_id', $user->id)
+            ->where('event_id', $eventId)
+            ->exists();
+
+        if ($alreadyJoined) {
+            return response()->json([
+                'success' => false,
+                'message' => 'You already joined this event.'
+            ], 409);
+        }
+
+        JoinEvent::create([
+            'user_id' => $user->id,
+            'event_id' => $eventId,
+        ]);
+
         return response()->json([
-            'success' => false,
-            'message' => 'Unauthenticated.'
-        ], 401);
+            'success' => true,
+            'message' => 'You successfully joined the event!',
+        'join_count' => $user->joinedevent()->count()
+
+        ], 201);
     }
-
-    $event = Event::find($eventId);
-    if (!$event) {
-        return response()->json([
-            'success' => false,
-            'message' => 'Event not found.'
-        ], 404);
-    }
-
-    $alreadyJoined = JoinEvent::where('user_id', $user->id)
-        ->where('event_id', $eventId)
-        ->exists();
-
-    if ($alreadyJoined) {
-        return response()->json([
-            'success' => false,
-            'message' => 'You already joined this event.',
-            'join_count' => $event->joins()->count() // count of joins
-        ], 409);
-    }
-
-    JoinEvent::create([
-        'user_id' => $user->id,
-        'event_id' => $eventId,
-    ]);
-
-    // Return total join count
-    $joinCount = $event->joins()->count();
-
-    return response()->json([
-        'success' => true,
-        'message' => 'You successfully joined the event!',
-        'join_count' => $joinCount
-    ], 201);
-}
 
 
     public function date(Request $request)
